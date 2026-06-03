ce25864a92fd9f3c0bcf7ed6a5b1f24081d552c1
Prasanth BJ <prasanthbj65@gmail.com>
2026-06-03 22:06:10 +0530

    Backend Project Setup

---
+++ src/main/java/com/projects/docubotpro/DocubotProApplication.java
@@ -0,0 +1,13 @@
+package com.projects.docubotpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocubotProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocubotProApplication.class, args);
+    }
+
+}
